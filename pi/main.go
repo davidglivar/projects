7ea40c5b3eef8832b1e6cff3f97a052c2db71d8e
David Glivar <davidglivar@gmail.com>
2014-03-14 11:40:02 -0700

    fix to pi solution

--- pi/main.go
+++ pi/main.go
@@ -11,15 +11,16 @@ import (
 
 func main() {
 	var place int
-	fmt.Println("How many decimals?")
+	fmt.Print("How many decimals? ")
 	_, err := fmt.Scan(&place)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 	if place > 50 {
 		fmt.Println("Try a smaller number")
 		return
 	}
-	pi = (4 * (4*math.Atan(1.0/5.0) - math.Atan(1.0/239.0)))
+	pi := (4 * (4*math.Atan(1.0/5.0) - math.Atan(1.0/239.0)))
 	fmt.Printf("%."+fmt.Sprintf("%d", place)+"f\n", pi)
 }
